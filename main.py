da6ea570eb50f9c431463f648af8634491bd0a58
v.belin <padoruactor@yandex.ru>
2025-06-10 13:58:49 +0300

    Base dicts ( SecLists)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import subprocess
 
+
 def run_ffuf(wordlist, url):
+    # Ensure URL contains FUZZ
+    if "FUZZ" not in url:
+        url = url.rstrip("/") + "FUZZ"
+
     try:
         result = subprocess.run(
             ['ffuf', '-w', wordlist, '-u', url],
@@ -8,16 +13,16 @@ def run_ffuf(wordlist, url):
             text=True,
             check=True
         )
-        # Print both stdout and stderr
         print("FFUF Output:")
-        print(result.stdout)
-        print(result.stderr)
+        print(result.stderr)  # Progress info
+        print(result.stdout)  # Results
         return result.stdout
     except subprocess.CalledProcessError as e:
         print(f"FFUF failed with error: {e.stderr}")
         return None
 
+
 # Usage
-wordlist = r"C:\Users\Vyacheslav\PycharmProjects\FFuF_Trainee\Dicts\DB&SQLInj\OracleDB-SID.txt"
-target_url = "http://guap.ruFUZZ"
+wordlist = r"C:\Users\Vyacheslav\PycharmProjects\FFuF_Trainee\Dicts\DBSQLInj\MSSQL-Enumeration.fuzzdb.txt"
+target_url = "http://guap.ru"  # FUZZ will be added automatically
 run_ffuf(wordlist, target_url)
